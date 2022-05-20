b6c979cd3ecce171f01664b2586d14fef308a5ac
andras nemeth <andras.nemeth02@gmail.com>
2022-05-20 14:16:48 +0100

    Bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # get 'lists/new'
   # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
